dbbbeaee5c118f00f40570b18eb697ff47a53051
MUKILTEO\1549673 <apollorevermann@gmail.com>
2025-10-23 12:30:36 -0700

    Progress on animation. Seperated animations.

--- Assets/Player_Movement.cs
+++ Assets/Player_Movement.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class Player_Movement : MonoBehaviour
 {
+    public Rigidbody2D myRigidbody;
+    public float movespeed;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +13,21 @@ public class Player_Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.UpArrow) == true)
+        {
+            myRigidbody.linearVelocityY = movespeed;
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow) == true)
+        {
+            myRigidbody.linearVelocityX = -movespeed;
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow) == true)
+        {
+            myRigidbody.linearVelocityX = movespeed;
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow) == true)
+        {
+            myRigidbody.linearVelocityY = -movespeed;
+        }
     }
 }
