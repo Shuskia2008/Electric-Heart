c8ef5bc7ef3cfd19c75ec6c02d769ed1fec171b0
MUKILTEO\1549673 <apollorevermann@gmail.com>
2025-11-03 13:25:06 -0800

    WallJump improved I guess

--- Assets/Player_Movement.cs
+++ Assets/Player_Movement.cs
@@ -10,6 +10,7 @@ public class Player_Movement : MonoBehaviour
     public WallChecker l;
     public WallChecker r;
     public bool hasTouchedGrass = false;
+    public float UnclingTimer;
     public float ClingTimer;
     public float LMoveDisabled;
     public float RMoveDisabled;
@@ -23,6 +24,7 @@ public class Player_Movement : MonoBehaviour
     void Start()
     {
         //Wall Jump Clinging -START-TIME
+        UnclingTimer = 10; 
         ClingTimer = 10;
         LMoveDisabled = 10;
         RMoveDisabled = 10;
@@ -37,12 +39,15 @@ public class Player_Movement : MonoBehaviour
         RMoveDisabled = RMoveDisabled + Time.deltaTime;
         //Wall Jump Clinging -UPDATE-TIME
         ClingTimer = ClingTimer + Time.deltaTime;
+        //Wall Jump Unclinging -UPDATE-TIME
+        UnclingTimer = UnclingTimer + Time.deltaTime;
         //Wall Jump Clinging -TIMER-START-RESET
         if (r.WallDetection == true && Input.GetKey(KeyCode.RightArrow) && hasTouchedGrass == true && GetComponentInChildren<GroundChecker>().GroundDetection == false)
         {
             ClingTimer = 0;
             hasTouchedGrass = false;
             myRigidbody.gravityScale = 0;
+            Debug.Log("Clinging Right");
         }
         if (l.WallDetection == true && Input.GetKey(KeyCode.LeftArrow) && hasTouchedGrass == true && GetComponentInChildren<GroundChecker>().GroundDetection == false)
         {
@@ -51,33 +56,41 @@ public class Player_Movement : MonoBehaviour
             myRigidbody.gravityScale = 0;
         }
         //Wall Jump LMove -TIMER-START-RESET-----------------------------------------------------
-        if ((Input.GetKeyDown(KeyCode.UpArrow) == true || Input.GetKeyDown(KeyCode.Space) == true) && l.WallDetection == true && GetComponentInChildren<GroundChecker>().GroundDetection == false)
+        if ((Input.GetKey(KeyCode.UpArrow) == true || Input.GetKey(KeyCode.Space) == true) && l.WallDetection == true && GetComponentInChildren<GroundChecker>().GroundDetection == false)
         {
             LMoveDisabled = 0;
         }
         if (Input.GetKey(KeyCode.LeftArrow) == true && LMoveDisabled < 0.5f)
         {
-            myRigidbody.linearVelocityX = -movespeed * 0.5f;
+            //NOTHING
         }
         //Wall Jump RMove -TIMER-START-RESET-----------------------------------------------------------------
-        if ((Input.GetKeyDown(KeyCode.UpArrow) == true || Input.GetKeyDown(KeyCode.Space) == true) && r.WallDetection == true && GetComponentInChildren<GroundChecker>().GroundDetection == false)
+        if ((Input.GetKey(KeyCode.UpArrow) == true || Input.GetKey(KeyCode.Space) == true) && r.WallDetection == true && GetComponentInChildren<GroundChecker>().GroundDetection == false)
         {
             RMoveDisabled = 0;
         }
         if (Input.GetKey(KeyCode.RightArrow) == true && RMoveDisabled < 0.5f)
         {
-            myRigidbody.linearVelocityX = movespeed * 0.5f;
+            //NOTHING
         }
         //Wall Jump Clinging -TIMER-FINISH
         if (ClingTimer > 2f)
         {
             myRigidbody.gravityScale = 5;
-            hasTouchedGrass = true;
+            
         }
-        if (hasTouchedGrass = false && (Input.GetKeyDown(KeyCode.LeftArrow) == true || Input.GetKeyDown(KeyCode.RightArrow) == true || Input.GetKeyDown(KeyCode.UpArrow) == true || Input.GetKeyDown(KeyCode.Space) == true))
+        if (hasTouchedGrass == false && (Input.GetKeyDown(KeyCode.LeftArrow) == true || Input.GetKeyDown(KeyCode.RightArrow) == true || Input.GetKeyDown(KeyCode.UpArrow) == true || Input.GetKeyDown(KeyCode.Space) == true))
         {
             myRigidbody.gravityScale = 5;
-            hasTouchedGrass = true;
+            
+        }
+        if (hasTouchedGrass == false && (Input.GetKeyUp(KeyCode.LeftArrow) == true || Input.GetKeyUp(KeyCode.RightArrow) == true || Input.GetKeyUp(KeyCode.UpArrow) == true || Input.GetKeyUp(KeyCode.Space) == true))
+        {
+            UnclingTimer = 0;
+        }
+        if (UnclingTimer > 0.3)
+        {
+        myRigidbody.gravityScale = 5;
         }
         //Wall Jump LMove -TIMER-FINISH------------------------------------------------------------------------
         if (LMoveDisabled > 0.5f)
@@ -101,11 +114,11 @@ public class Player_Movement : MonoBehaviour
         //Wall Jump -PUSH
         if ((Input.GetKeyDown(KeyCode.UpArrow) == true || Input.GetKeyDown(KeyCode.Space) == true) && l.WallDetection == true && GetComponentInChildren<GroundChecker>().GroundDetection == false)
         {
-            myRigidbody.linearVelocityX = movespeed;
+            myRigidbody.linearVelocityX = movespeed * 1.2f;
         }
         if ((Input.GetKeyDown(KeyCode.UpArrow) == true || Input.GetKeyDown(KeyCode.Space) == true) && r.WallDetection == true && GetComponentInChildren<GroundChecker>().GroundDetection == false)
         {
-            myRigidbody.linearVelocityX = -movespeed;
+            myRigidbody.linearVelocityX = -movespeed * 1.2f;
         }
         //Wall Jump -LIMITER-JUMP
         if ((Input.GetKeyDown(KeyCode.UpArrow) == true || Input.GetKeyDown(KeyCode.Space) == true) && GetComponentInChildren<GroundChecker>().GroundDetection == false && (l.WallDetection == true || r.WallDetection == true))
@@ -156,6 +169,7 @@ public class Player_Movement : MonoBehaviour
         if (collision.gameObject.CompareTag("ground") && GetComponentInChildren<GroundChecker>().GroundDetection == true && (l.WallDetection == false && r.WallDetection == false))
         {
             hasTouchedGrass = true;
+            Debug.Log("Landed");
         }
     
     }
